8b263b960dc38497cd244dd8b940cff9c8d38623
virtualpatterns <code.virtualpatterns@gmail.com>
2020-10-17 20:36:26 -0400

    post-test

---
+++ distributable-esmodule/test/install.test.js
@@ -0,0 +1,23 @@
+import Path from 'path';
+import Test from 'ava';
+import URL from 'url';
+
+Test('Error(string)', test => {
+
+  let error = new Error('error');
+
+  let errorStack = error.stack.split('\n');
+  let [, stackItem] = errorStack;
+
+  let pattern = /at (.+):(\d+):(\d+)/i;
+  let [, errorPath, errorLineNumber, errorColumnNumber] = stackItem.match(pattern);
+
+  test.log(`'${Path.relative('', errorPath)}' on line ${errorLineNumber}, column ${errorColumnNumber}`);
+
+  let filePath = URL.fileURLToPath(import.meta.url);
+  filePath = filePath.replace(/distributable-esmodule/i, 'source-esmodule');
+
+  test.is(errorPath, filePath);
+
+});
+//# sourceMappingURL=install.test.js.map
